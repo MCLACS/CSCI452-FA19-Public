620ef147d51f80dd7a980938c48ed1eb94da1798
Jacob Dionne <47004564+jd6378@users.noreply.github.com>
2019-10-01 13:03:25 -0400

    Email Validation Check
    validateEmail Function

--- server.js
+++ server.js
@@ -107,6 +107,19 @@ function getSnippets(req, res)
         });
 }
 
+function validateEmail(email) 
+{
+  if (email == undefined)
+  {
+    return false;
+  }
+  else
+  {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+}
+
 function validatePassword(pass)
 {
   if (pass == undefined)
