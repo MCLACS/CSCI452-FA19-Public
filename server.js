855bac7b33d1a1316e512fd67fc44965dde52da2
Jacob Dionne <47004564+jd6378@users.noreply.github.com>
2019-10-15 13:25:19 -0400

    Updated validatePassword() regex
    Issue 77
    Issue 68

--- server.js
+++ server.js
@@ -353,7 +353,7 @@ function validatePassword(pass, callback)
   }
   else
   {
-    var re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    var re = /^(?=.[ -0-z])(?=.\d)[ -0-z\d]{8,59}$/;
     callback(re.test(pass));
   }
 }
